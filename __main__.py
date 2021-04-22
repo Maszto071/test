1f8dd697b449495fa28c594071d58797bede9c7f
Maszto071 <tpodgorski@mmm.com>
2021-04-22 12:59:15 +0200

    Added 'title_it' Fnc

--- __main__.py
+++ __main__.py
@@ -6,11 +6,16 @@ def check_dir():
     return os.getcwd()
 
 
-def example():
+def example(name):
     # redundant comment
     pass
+
+
+def title_it(name):
+    return name.title()
 
 
 if __name__ == "__main__":
     print(check_dir())
     print('hello world')
+    print(title_it("hello world!"))
